730293dbce670b9c1e1a40898a505117ea2cb8d1
kikefranco <luis.franco@wizeline.com>
2023-08-17 15:48:57 -0500

    The header css styling is finished missing logo

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
+
 import './css/style.css'
 import './css/tablet.css'
 import './css/desktop.css'
@@ -8,24 +11,33 @@ import logo from './img/logo.png'
 const Header = () => {
   return(
     <header className="header">
+
       <div className="header__logo">
         {/* <img src={logo}></img> */}
         LOGO
       </div>
+
       <div className="header__search">
-        <input type="text" placeholder="Search..." className="header_search-input"/>
-        <button className="header__search-button">Search</button>
+        <div className="header__search-container">
+          <input type="text" placeholder="Search..." className="header__search-input"/>
+          <div className="header__search-icon-container">
+              <MagnifyingGlassIcon className="header__search-icon" />
+          </div>
+        </div>
       </div>
+
       <nav className="header__nav">
         <ul className="header__nav-list">
           <li className="header__nav-item">
-            <a href="#" className="header__nav-link">Option 1</a>
+            <ShoppingCartIcon className="nav-cart-icon" />
           </li>
           <li className="header__nav-item">
-            <a href="#" className="header__nav-link">Option 2</a>
-          </li>
-          <li className="header__nav-item">
-            <a href="#" className="header__nav-link">Option 3</a>
+              <img
+                className="nav-account-img" 
+                src={"https://lh3.googleusercontent.com/ogw/AGvuzYbYN3iKpi6pZVDNSBfh3ysqX6LNAIuovaKmAupW6Q=s32-c-mo"}
+              ></img>
+            {/* <div className="nav-account-container">
+            </div> */}
           </li>
         </ul>
       </nav>
